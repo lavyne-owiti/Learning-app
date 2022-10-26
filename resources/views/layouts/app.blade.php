42ec0a834e1577a9a1ca9fc9cafab3c81e5a5403
lavyne-owiti <owitilavyne@gmail.com>
2022-10-26 10:44:12 +0300

    register

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="bg-gray-200 mb-6">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href=""  class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            @auth
+                <li>
+                    <a href="" class="p-3">Lavyne Owiti</a>
+                </li>
+                <li>
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit" class="p-3">Logout</button>
+                    </form>
+                </li>
+            
+            @endauth
+
+            @guest
+                <li>
+                    <a href="{{ route('login') }}"  class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+        
+            @endguest
+
+        </ul>
+    </nav>
+   
+    @yield('content')
+</body>
+</html>
+
+
